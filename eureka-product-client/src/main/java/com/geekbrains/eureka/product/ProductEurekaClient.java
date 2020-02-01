60b95fe01127beadb944c22408313d5f02f6778b
rrmice <rrmice@yandex.ru>
2020-02-02 00:02:37 +0300

    cloud

---
+++ eureka-product-client/src/main/java/com/geekbrains/eureka/product/ProductEurekaClient.java
@@ -0,0 +1,11 @@
+package com.geekbrains.eureka.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductEurekaClient {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductEurekaClient.class, args);
+    }
+}
